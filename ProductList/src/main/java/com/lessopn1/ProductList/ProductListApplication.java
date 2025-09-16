41bb65f60c16680e02b6c17752ce7b9e27c2eb9d
Rimjhim <rimjhimsaxena535@gmail.com>
2025-09-16 09:57:25 +0530

    first commit

---
+++ ProductList/src/main/java/com/lessopn1/ProductList/ProductListApplication.java
@@ -0,0 +1,13 @@
+package com.lessopn1.ProductList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListApplication.class, args);
+	}
+
+}
